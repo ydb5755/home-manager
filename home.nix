d0df309854dd31aefa36b4c628ed7790b28f7e0e
Yisroel Baum <yisroel.d.baum@gmail.com>
2026-03-29 17:27:43 +0300

    add programs
    git, starship, bash, and opencode

--- home.nix
+++ home.nix
@@ -28,6 +28,29 @@
 
   programs.home-manager.enable = true;
 
+  programs.git = {
+    enable = true;
+    userName = "Yisroel Baum";
+    userEmail = "yisroel.d.baum@gmail.com";
+  };
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your custom bashrc here
+    bashrcExtra = ''
+      export PATH=/home/yisroel/.opencode/bin:$PATH
+    '';
+  };
+  programs.opencode.enable = true;
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
